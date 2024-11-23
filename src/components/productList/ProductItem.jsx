1f3d8811c68f8d4c2a5952264c74e0441e675bbc
dungvq <dung.vuquoc0102@gmail.com>
2024-11-23 18:06:09 +0700

    Fix ProductItem.jsx

--- src/components/productList/ProductItem.jsx
+++ src/components/productList/ProductItem.jsx
@@ -7,13 +7,13 @@ export default function ProductItem({ product }) {
 				{/* title */}
 				<div className="px-2 pt-4 font-bold line-clamp-2 min-h-[64px] border-t-[1px] border-gray-200">{product.name}</div>
 				{/* short description */}
-				<div className="text-sm m-2 line-clamp-2 min-h-[40px] text-gray-500">Mô tả: {product.short_description}</div>
+				<div className="text-sm m-2 min-h-[140px] text-gray-500">Mô tả: {product.short_description}</div>
 				{/* sku */}
 				<div className="m-2 text-sm text-gray-500">SKU: {product.sku}</div>
 				{/* material */}
 				<div className="m-2 text-sm line-clamp-1 text-gray-500">Chất liệu: {product.materials || "Đang cập nhật"}</div>
 				{/* instruction */}
-				<div className="m-2 text-sm line-clamp-2 min-h-[40px] text-gray-500">Hướng dẫn: {product.instruction || "Đang cập nhật"}</div>
+				<div className="m-2 text-sm min-h-[120px] text-gray-500">Hướng dẫn: {product.instruction || "Đang cập nhật"}</div>
 				<div className="m-2 flex justify-between text-sm">
 					{/* price */}
 					<div className="font-bold whitespace-nowrap">
