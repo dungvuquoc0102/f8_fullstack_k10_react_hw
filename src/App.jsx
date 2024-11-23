7842a46a8f532755d522a3f317684a3d7a578b06
dungvq <dung.vuquoc0102@gmail.com>
2024-11-23 12:58:12 +0700

    Fix link in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import "./App.css";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import ProductList from "./components/productList/productList";
+import ProductList from "./components/productList/ProductList";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <ProductList />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<ProductList />
+			<Footer />
+		</>
+	);
 }
 
 export default App;
